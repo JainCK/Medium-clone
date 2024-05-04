efe3eb4ac325961ed71eafeea50f6341a026814a
Jain K <kuriakosejain456@gmail.com>
2024-05-04 21:40:35 +0530

    added zod infer

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign, verify } from "hono/jwt";
+import { createPostInput, updatePostInput } from "@jainck88/jmedium-app";
 
 export const blogRouter = new Hono<{
   Bindings: {
