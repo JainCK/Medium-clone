ecb397b9146aa1c6c5ea9cd2f72fcfb185ae0377
Jain K <kuriakosejain456@gmail.com>
2024-05-05 23:24:44 +0530

    styling done

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,5 +1,22 @@
 const Quote = () => {
-  return <div className="h-screen bg-slate-200">hi Quote</div>;
+  return (
+    <div className="h-screen bg-slate-200 flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className=" text-3xl font-bold">
+            "The customer service i recieved was exceptional. the support team
+            went above and beyond to address my concerns"
+          </div>
+          <div className="max-w-md text-xl font-semibold mt-5">
+            Jules Winnfield
+          </div>
+          <div className="max-w-md text-sm font-light text-slate-400">
+            CEO | Acme Inc
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Quote;
