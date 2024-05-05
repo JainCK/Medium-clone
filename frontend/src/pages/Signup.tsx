73f55aa0bfa45128477861f5dc0b1806ccb031fb
Jain K <kuriakosejain456@gmail.com>
2024-05-05 23:24:54 +0530

    responsiveness added

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,17 @@
+import Auth from "../components/Auth";
 import Quote from "../components/Quote";
 
 const Signup = () => {
   return (
     <>
-      <Quote />
+      <div className="grid grid-cols-2">
+        <div>
+          <Auth />
+        </div>
+        <div className="invisible lg:visible">
+          <Quote />
+        </div>
+      </div>
     </>
   );
 };
