cb6cec89756e129ac23fbbe7ee0060398efd979d
Jain K <kuriakosejain456@gmail.com>
2024-05-05 22:28:07 +0530

    Componenets init

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,5 @@
+const Quote = () => {
+  return <div className="h-screen bg-slate-200">hi Quote</div>;
+};
+
+export default Quote;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import Quote from "../components/Quote";
+
+const Signup = () => {
+  return (
+    <>
+      <Quote />
+    </>
+  );
+};
+
+export default Signup;
