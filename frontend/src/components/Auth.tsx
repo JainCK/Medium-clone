f8aba75f0c2d7578c3ee6d7fd3caa7aacaf0dfb6
Jain K <kuriakosejain456@gmail.com>
2024-05-06 23:15:55 +0530

    component style

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -16,24 +16,30 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
           <div className="px-10">
             <div className="text-3xl font-extrabold">Create an account</div>
             <div className="text-slate-400">
-              Already have an account?
-              <Link className="pl-2 underline" to={"/signin"}>
-                Login
+              {type === "signin"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signin" ? "/signup" : "/signin"}
+              >
+                {type === "signin" ? "Sign up" : "Sign in"}
               </Link>
             </div>
           </div>
           <div className="pt-10">
-            <LabelInput
-              label="Username"
-              type={"name"}
-              placeholder="John Doe"
-              onChange={(e) => {
-                setPostInputs({
-                  ...postInputs,
-                  name: e.target.value,
-                });
-              }}
-            />
+            {type === "signup" ? (
+              <LabelInput
+                label="Name"
+                placeholder="Harkirat Singh..."
+                onChange={(e) => {
+                  setPostInputs({
+                    ...postInputs,
+                    name: e.target.value,
+                  });
+                }}
+              />
+            ) : null}
             <LabelInput
               label="Email"
               type={"email"}
@@ -56,6 +62,13 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
                 });
               }}
             />
+            <button
+              // onClick={sendRequest}
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign up" : "Sign in"}
+            </button>
           </div>
         </div>
       </div>
